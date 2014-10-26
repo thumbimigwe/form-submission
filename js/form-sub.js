26fe6a148f7c04d1d72821cbabcdcce9ca4e98e7
shims506 <shims506@gmail.com>
2014-10-25 21:57:23 -0500

    adding more fields in the bootstrap

--- js/form-sub.js
+++ js/form-sub.js
@@ -140,6 +140,23 @@ $(document).ready(function() {
                     }
                 }
             },
+                name: {
+                    message: 'The name is not valid',
+                    validators: {
+                        notEmpty: {
+                            message: 'The name is required and cannot be empty'
+                        },
+                        stringLength: {
+                            min: 6,
+                            max: 30,
+                            message: 'The name must be more than 6 and less than 30 characters long'
+                        },
+                        regexp: {
+                            regexp: /^[a-zA-Z0-9_]+$/,
+                            message: 'The name can only consist of alphabetical, number and underscore'
+                        }
+                    }
+                },
             email: {
                 validators: {
                     notEmpty: {
