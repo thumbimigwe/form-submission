2b4b6f259d5b13e932236e815f76ef389f417c4d
Shims <shims506@gmail.com>
2015-01-21 18:28:52 -0600

    completed the for loop

--- js/firebase-initialize.js
+++ js/firebase-initialize.js
@@ -7,10 +7,12 @@ function createBranch(events_in, firebase_id){
     var events_in_count = events_in.length,
         i = 0;
     //create firebase ref here
+    var firebase_id = new Firebase(firebase_id);
 
+    //this will initialize the firebase
+    //iterate through array and post 0 to count of each event.
     for(;i<events_in_count;i++) {
-
-
+        firebase_id.child(events_in[i]).set({Count: 0});
     }
-
+    alert('Complete')
 }
