0fdc3a4ff407a94f7b18b57a9ad1a52cb04b985e
Shims <shims506@gmail.com>
2015-01-25 18:23:13 -0600

    added a admin page: one problem is that the function runs to quickly for fb to post

--- js/firebase-initialize.js
+++ js/firebase-initialize.js
@@ -2,17 +2,24 @@
  * Created by shimsware on 1/21/15.
  */
 
-function createBranch(events_in, firebase_id){
+function createBranch(){
+    //events_in must be in format event_1_334,event_2_234,etc...
 
-    var events_in_count = events_in.length,
+
+
+
+    var events_in_array = $("#events_in").val().split(",");
+    var events_in_count = events_in_array.length,
         i = 0;
     //create firebase ref here
-    var firebase_id = new Firebase(firebase_id);
+    var firebase_idnew = new Firebase($("#firebase_id").val());
+
+    alert(events_in_array+'    '+firebase_idnew);
 
     //this will initialize the firebase
     //iterate through array and post 0 to count of each event.
     for(;i<events_in_count;i++) {
-        firebase_id.child(events_in[i]).set({Count: 0});
+        firebase_idnew.child(events_in_array[i]).set({Count: 0});
     }
     alert('Complete')
 }
